2784bd36b73b025bedd9c45a121eb691be95d5da
githubnm <githubnm@gmail.com>
2021-10-28 06:45:52 -0500

    Сделал 5.1 ( посмотрел ответ )

--- exercises/05_basic_scripts/answer_task_5_1b.py
+++ exercises/05_basic_scripts/answer_task_5_1b.py
@@ -50,7 +50,9 @@ london_co = {
         "routing": True,
     },
 }
-name = input('Enter name: ')
-param = ','.join(london_co[name].keys())
-parametr = input(f"Enter parametr({param}): ")
-print(london_co[name][parametr])
+
+device = input("Введите имя устройства: ")
+params = ", ".join(london_co[device].keys())
+parameter = input(f"Введите имя параметра ({params}): ")
+
+print(london_co[device][parameter])
