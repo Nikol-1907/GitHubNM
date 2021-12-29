56b6bac335b96481109b206f7d7c0a5d585ba1be
githubnm <githubnm@gmail.com>
2021-12-29 07:27:07 -0600

    Сделал 6.2a

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,31 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip = input('Enter IP_address: ')
+octet = ip.split(".")
+correct_ip = True
+if len(octet) != 4:
+	correct_ip = False
+for octets in octet:
+	if not (octets.isdigit() and int(octets) in range(256)):
+		correct_ip = False
+		break
+if not correct_ip:
+	print('неправильный ip-адрес')
+	
+else:
+	oct_ip = [int(i) for i in octet]
+	if oct_ip[0] in range(1,223):
+		print('unicast')
+	elif oct_ip[0] in range(224,239):
+		print('multicast')
+	elif ip == '255.255.255.255':
+		print('local broadcast')
+	elif ip == '0.0.0.0':
+		print('unassigned')
+	else:
+		print('unused')
+ 
+	
+	
